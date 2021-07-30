b9b0acb65ca1591ce51e330f210e60cf9d6761cd
Iuri Magnago <iurimp91@gmail.com>
2021-07-30 10:35:49 -0300

    Get categories from server

--- src/components/form/CategoryInput.js
+++ src/components/form/CategoryInput.js
@@ -1,18 +1,28 @@
+import { useEffect, useState } from "react";
 import { Container } from "../../styles/FormFieldsContainer";
+import axios from "axios";
 
 export default function CategoryInput({ setCategory }) { 
-    const categories = [
-        { id: 1, name: "P1"},
-        { id: 2, name: "P2"},
-        { id: 3, name: "P3"},
-    ];
+    const [categoriesList, setCategoriesList] = useState();
+
+    useEffect(() => {
+        const request = axios.get("http://localhost:4000/categories");
+
+        request.then((response) => {
+            setCategoriesList(response.data);
+        });
+
+        request.catch((error) => {
+            alert("Algo deu errado com sua requisição, atualize a página, por favor.");
+        });
+    }, []);
     
     return(
         <Container>
             <span>categoria</span>
             <select required id="categories" onChange={(e) => setCategory(e.target.value)} defaultValue="" >
                 <option disabled ></option>
-                {categories.map(category => (
+                {categoriesList?.map(category => (
                     <option key={category.id} value={category.id}>{category.name}</option>
                 ))}
             </select>
